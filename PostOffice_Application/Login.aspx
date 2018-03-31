3b7bc57baa4b1a66ff6c684aa8ba79345f2ea53b
Jonathan Davis <jedavis13579@sbcglobal.net>
2018-03-31 11:04:02 -0500

    Created Login Page

---
+++ PostOffice_Application/Login.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="PostOffice_Application.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        body{
+            background-color:cadetblue;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="margin:auto;border:5px solid black">
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label></td>
+                    <td>
+                        <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label></td>
+                    <td>
+                        <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td>
+                        
+                        <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="Post_Office" DataTextField="User_Type_Name" DataValueField="User_Type_Name">
+                        </asp:DropDownList>
+                        <asp:SqlDataSource ID="Post_Office" runat="server" ConnectionString="<%$ ConnectionStrings:Post_OfficeConnectionString %>" SelectCommand="SELECT [User_Type_Name] FROM [USER_TYPE] ORDER BY [User_Type_Name]"></asp:SqlDataSource>
+                        
+                        </td>
+                    <td>
+                        <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Label ID="lblLoginError" runat="server" Text="Invalid login info" ForeColor="Red"></asp:Label></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PostOffice_Application/Login.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PostOffice_Application
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblLoginError.Visible = false;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var cb = new SqlConnectionStringBuilder();
+                cb.DataSource = "team-4-post-office-dbs.database.windows.net";
+                cb.UserID = "ServerAdmin";
+                cb.Password = "UmaThurman18";
+                cb.InitialCatalog = "Post_Office";
+
+                using (SqlConnection connection = new SqlConnection(cb.ConnectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT COUNT(1) FROM LOGIN WHERE Username=@Username AND Password=@Password AND User_Type_Name=@UserType";
+                    SqlCommand checkCredentials = new SqlCommand(query, connection);
+                    checkCredentials.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                    checkCredentials.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                    checkCredentials.Parameters.AddWithValue("@UserType", DropDownList1.Text.Trim());
+                    int count = Convert.ToInt32(checkCredentials.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        Session["Username"] = txtUsername.Text.Trim();
+                        string display = "Login Successful!";
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + display + "');", true);
+                    }
+                    else { lblLoginError.Visible = true; }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
