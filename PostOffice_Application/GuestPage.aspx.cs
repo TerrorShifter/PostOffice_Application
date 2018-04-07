3d56af6fd913294539f5a267f8c47e20a26c755c
Kunle Eniola <eniolakunle10@gmail.com>
2018-04-07 18:50:41 -0500

    Beginnings of Initial Page

---
+++ PostOffice_Application/GuestPage.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+namespace PostOffice_Application
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx"); 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(""); //will redirect to sign up page once complete
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
