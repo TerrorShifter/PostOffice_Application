7435488d8091234cddf3990ed68216522ae20486
Luis Flores <luis.f1220@gmail.com>
2018-04-21 18:37:06 -0500

    Added a page for employee route creation + front end.

---
+++ PostOffice_Application/Employee/EmployeeCreateRoute.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  MasterPageFile="~/Employee/EmployeeMaster.Master" CodeBehind="EmployeeCreateRoute.aspx.cs" Inherits="PostOffice_Application.Employee.EmployeeCreateRoute" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Create New Delivery Route</h1>
+    <p>
+        Starting Address:
+        <asp:TextBox ID="txtStartingAddr" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+    </p>
+    <p>
+        Destination Address:
+        <asp:TextBox ID="txtDestinationAddr" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Driver ID:
+        <asp:TextBox ID="txtDriverID" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Label ID="lblCreateRouteInfo" runat="server" Visible="False"></asp:Label>
+        <asp:Button ID="btnCreateRoute" runat="server" OnClick="btnCreateRoute_Click" Text="Create" />
+    </p>
+    <p>
+        &nbsp;</p>
+</asp:Content>
---
+++ PostOffice_Application/Employee/EmployeeCreateRoute.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostOffice_Application.Employee
+{
+    public partial class EmployeeCreateRoute : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateRoute_Click(object sender, EventArgs e)
+        {
+            if(txtDestinationAddr.Text=="")
+            {
+                lblCreateRouteInfo.ForeColor = System.Drawing.Color.Red;
+                lblCreateRouteInfo.Text = "Please enter a destination";
+                lblCreateRouteInfo.Visible = true;
+            }
+            else if (txtDriverID.Text=="")
+            {
+                lblCreateRouteInfo.ForeColor = System.Drawing.Color.Red;
+                lblCreateRouteInfo.Text = "Please enter a driver ID";
+                lblCreateRouteInfo.Visible = true;
+            }
+            else if(txtStartingAddr.Text=="")
+            {
+                lblCreateRouteInfo.ForeColor = System.Drawing.Color.Red;
+                lblCreateRouteInfo.Text = "Please enter a starting address";
+                lblCreateRouteInfo.Visible=true;
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
