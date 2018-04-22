59dca63ce1e54fcbecfe9fc3b7a444841b98f5ab
Jonathan Davis <jedavis13579@sbcglobal.net>
2018-04-22 17:47:35 -0500

    Removed SignUp Page

--- PostOffice_Application/Default.aspx.cs
+++ PostOffice_Application/Default.aspx.cs
@@ -87,7 +87,6 @@ namespace PostOffice_Application
                         lblShipped.Visible = true;
                         conn.Close();
 
-
                     }
                     
                 }
